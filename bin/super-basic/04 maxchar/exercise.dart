dca1cfb3b7f900aeaf5bfe65f922681f29f8ad27
Quang Long <longle2243@gmail.com>
2022-07-15 11:19:32 +0700

    1 2 3 4 6 complete

--- bin/super-basic/04 maxchar/exercise.dart
+++ bin/super-basic/04 maxchar/exercise.dart
@@ -1,4 +1,3 @@
-
 // --- Directions
 // Given a string, return the character that is most
 // commonly used in the string.
@@ -6,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+import 'dart:math';
+
 String maxChar(String input) {
-  
+  final list = input.split("");
+
+  var map = Map();
+  list.forEach((element) {
+    if (!map.containsKey(element)) {
+      map[element] = 1;
+    } else {
+      map[element] += 1;
+    }
+  });
+
+  //print(map);
+  int reslut =
+      map.values.reduce((value, element) => value > element ? value : element);
+  //print(reslut);
+  String result1 = "";
+  map.forEach((key, value) {
+    if (value == reslut) {
+      //print(key);
+      result1 = key;
+    }
+  });
+  return result1;
 }
--- bin/super-basic/06 chunk/exercise.dart
+++ bin/super-basic/06 chunk/exercise.dart
@@ -9,5 +9,24 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 List<List<int>> chunk(List<int> array, size) {
-  
+  List<List<int>> arr1 = [];
+  int n = size;
+  print(array.length);
+  for (var i = 0; i < array.length; i = i + n) {
+    if ((i + n) > array.length) {
+      List<int> arr = array.sublist(i, array.length);
+      arr1.add(arr);
+      //print(arr);
+      break;
+    }
+    //print(i + n);
+    List<int> arr = array.sublist(i, i + n);
+    arr1.add(arr);
+    //print(arr);
+  }
+  return arr1;
 }
+
+// void main() {
+//   chunk([1, 2, 3, 4, 5, 6, 7, 8], 3);
+// }
