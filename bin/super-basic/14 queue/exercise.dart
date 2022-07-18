654af702c2b43b3aad31ebb2c14adbeb5567a191
Mona Lisa <longle2243@gmail.com>
2022-07-18 16:34:21 +0700

    14 16

--- bin/super-basic/14 queue/exercise.dart
+++ bin/super-basic/14 queue/exercise.dart
@@ -8,6 +8,21 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
+import 'dart:collection';
+
 class Queue {
-  
+  final queue = [];
+
+  void add(int value) {
+    this.queue.add(value);
+  }
+
+  int? remove() {
+    int? result;
+    if (queue.length > 0) {
+      result = queue.elementAt(0);
+      queue.removeAt(0);
+    }
+    return result;
+  }
 }
--- bin/super-basic/16 stack/exercise.dart
+++ bin/super-basic/16 stack/exercise.dart
@@ -11,5 +11,26 @@
 //   s.pop(); // returns 1
 
 class Stack {
-  
+  List stack = [];
+
+  void push(int value) {
+    this.stack.add(value);
+  }
+
+  int? pop() {
+    int? result;
+    if (stack.length > 0) {
+      result = stack.elementAt(stack.length - 1);
+      stack.removeLast();
+    }
+    return result;
+  }
+
+  int? peek() {
+    int? result;
+    if (stack.length > 0) {
+      result = stack.elementAt(stack.length - 1);
+    }
+    return result;
+  }
 }
